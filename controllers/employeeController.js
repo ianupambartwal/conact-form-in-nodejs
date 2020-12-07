3760180d3e28dcc3b214b12fe6b238686b7b57c1
Anupam Bartwal <45586404+ianupambartwal@users.noreply.github.com>
2020-12-07 12:02:38 +0530

    Update employeeController.js

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -61,21 +61,6 @@ function updateRecord(req, res) {
     });
 }
 
-
-router.get('/list', (req, res) => {
-    Employee.find((err, docs) => {
-        if (!err) {
-            res.render("employee/list", {
-                list: docs
-            });
-        }
-        else {
-            console.log('Error in retrieving employee list :' + err);
-        }
-    });
-});
-
-
 function handleValidationError(err, body) {
     for (field in err.errors) {
         switch (err.errors[field].path) {
@@ -91,24 +76,8 @@ function handleValidationError(err, body) {
     }
 }
 
-router.get('/:id', (req, res) => {
-    Employee.findById(req.params.id, (err, doc) => {
-        if (!err) {
-            res.render("employee/addOrEdit", {
-                viewTitle: "Update Employee",
-                employee: doc
-            });
-        }
-    });
-});
 
-router.get('/delete/:id', (req, res) => {
-    Employee.findByIdAndRemove(req.params.id, (err, doc) => {
-        if (!err) {
-            res.redirect('/employee/list');
-        }
-        else { console.log('Error in employee delete :' + err); }
-    });
-});
+
+
 
 module.exports = router;
